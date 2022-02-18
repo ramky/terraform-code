b4e6b31fbac9b73dc3fdfafdf1f599c2216beec0
Ram Iyer <ramky.iyer@gmail.com>
2022-02-18 01:07:08 -0500

    using variable for security group

--- kafka/main.tf
+++ kafka/main.tf
@@ -4,15 +4,39 @@ provider "aws" {
 
 resource "aws_instance" "zoo1" {
   ami           = "ami-0e472ba40eb589f49"
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   vpc_security_group_ids = [
-    "sg-0348c08568c802d73",
+    var.security_group,
   ]
   tags = {
     Name = "zoo1"
   }
 }
 
+# resource "aws_instance" "zoo2" {
+#   ami           = "ami-0e472ba40eb589f49"
+#   instance_type = "t2.small"
+#   vpc_security_group_ids = [
+#     var.security_group,
+#   ]
+#   tags = {
+#     Name = "zoo2"
+#   }
+# }
+
+# resource "aws_instance" "zoo3" {
+#   ami           = "ami-0e472ba40eb589f49"
+#   instance_type = "t2.small"
+#   vpc_security_group_ids = [
+#     sg-0be9fdd7e313fcfc8,
+#   ]
+#   tags = {
+#     Name = "zoo3"
+#   }
+# }
+
 output "ec2_global_ips" {
+  # value = [aws_instance.zoo1.*.public_ip, aws_instance.zoo2.*.public_ip, aws_instance.zoo3.*.public_ip]
   value = [aws_instance.zoo1.*.public_ip]
+
 }
---
+++ kafka/variables.tf
@@ -0,0 +1,5 @@
+variable "security_group" {
+  type        = string
+  description = "name of security group"
+  default     = "sg-0be9fdd7e313fcfc8"
+}
