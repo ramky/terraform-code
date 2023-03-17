b571e196f6303dc416a6dc1a0e8f4effcb6051f2
Ram Iyer <ramky.iyer@gmail.com>
2023-03-16 23:28:45 -0400

    adding http server

--- udemy-devops/httpd_on_ec2/main.tf
+++ udemy-devops/httpd_on_ec2/main.tf
@@ -1,3 +1,7 @@
+variable "aws_key_pair" {
+  default = "~/aws/aws_keys/udemy_test.pem"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -5,6 +9,9 @@ provider "aws" {
 resource "aws_security_group" "http_server_sg" {
   name = "http_server_sg"
   vpc_id = "vpc-6b29a012"
+  tags = {
+    name = "http_server_sg"
+  }
   ingress {
     from_port = 80
     to_port = 80
@@ -22,5 +29,28 @@ resource "aws_security_group" "http_server_sg" {
     to_port = 0
     protocol = -1
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami = "ami-0557a15b87f6559cf"
+  key_name = "udemy_test"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id = "subnet-e33fbdcf"
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y", // install httpd
+      "sudo httpd start", // start service
+      "echo Welcome to Virtual Server ${self.public_dns} | sudo tee /var/www/html/index.html" // create default file
+    ]
   }
 }
