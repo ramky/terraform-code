ba7d7eea28880ef2a6b236ad8ecc0d1433dbc54a
Ram Iyer <ramky.iyer@gmail.com>
2023-03-16 22:40:15 -0400

    adding new files to create sg for http server

---
+++ udemy-devops/httpd_on_ec2/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  vpc_id = "vpc-6b29a012"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
